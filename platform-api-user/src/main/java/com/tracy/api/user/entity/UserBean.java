dda120c1dfc6d397e3550fb2fb084f104fb14b85
pengcheng <pengcheng3211@gmail.com>
2018-06-08 10:51:32 +0800

    重新调整bean对象

--- platform-api-user/src/main/java/com/tracy/api/user/entity/UserBean.java
+++ platform-api-user/src/main/java/com/tracy/api/user/entity/UserBean.java
@@ -5,20 +5,21 @@ import lombok.Data;
 import java.io.Serializable;
 import java.util.Date;
 
-
 @Data
 public class UserBean implements Serializable{
 	
-	private long userId;
+	private String id;
 	
 	private String userName;
 	
 	private long userPhone;
 	
 	//这里展示了jackson封装好的以及自定义的对时间格式的转换方式
-	//后续对于一些复杂的转换可以自定义转换方式
 	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date createTime;
+
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date modifyTime;
 	
 	private int score;
 
