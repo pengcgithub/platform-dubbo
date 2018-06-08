9387885e21ca82a6352d2c122ece32fbc847a443
pengcheng <pengcheng3211@gmail.com>
2018-06-08 09:33:28 +0800

    数据库结构脚本

---
+++ sql/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE beauty_ssm;
+
+CREATE TABLE `t_user` (
+`id`  varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL ,
+`user_name`  varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL ,
+`user_phone`  int(11) NULL DEFAULT NULL ,
+`create_time`  datetime NOT NULL ,
+`modify_time`  datetime NOT NULL ,
+`score`  int(5) NOT NULL ,
+PRIMARY KEY (`id`)
+);
