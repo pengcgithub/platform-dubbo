8e4006b4cc3de7806909216e224775e3a69a768c
pengcheng <pengcheng3211@gmail.com>
2018-08-27 16:02:08 +0800

    solr service调整

--- platform-service-search/src/main/java/com/tracy/service/api/userSearch/service/impl/UserSearchServiceImpl.java
+++ platform-service-search/src/main/java/com/tracy/service/api/userSearch/service/impl/UserSearchServiceImpl.java
@@ -3,8 +3,9 @@ package com.tracy.service.api.userSearch.service.impl;
 import com.tracy.api.search.service.userSearch.UserSearchService;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,18 +19,28 @@ import org.springframework.stereotype.Service;
 @Service(value = "userSearchService")
 public class UserSearchServiceImpl implements UserSearchService {
 
+//    @Autowired
+//    private SolrTemplate solrTemplate;
+
     @Autowired
-    private HttpSolrClient client;
+    private SolrClient solrClient;
 
     public Boolean importAllItems() {
         return null;
     }
 
     public String searchItems(String queryString, Integer page, Integer rows) throws Exception {
-        SolrQuery solr = new SolrQuery();
-        solr.setQuery("productName:苹果");
-        QueryResponse queryResponse = client.query(solr);
-        queryResponse.getResults();
+//        SimpleQuery simpleQuery = new SimpleQuery("*:*");
+//        Map<String, Object> solrItems = solrTemplate.query("store_v1", simpleQuery, Map.class);
+
+        SolrQuery query = new SolrQuery();
+        query.set("q", "*:*");
+        QueryResponse response = solrClient.query("store_v1", query);
+        SolrDocumentList list = response.getResults();
+
+        for (SolrDocument solrDocument : list) {
+            System.out.println(solrDocument.get("belongName"));
+        }
 
         return null;
     }
